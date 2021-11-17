0295ba70785bc96a7ba41992d3ced45ab0a76dc7
Cosmin Apreutesei <cosmin.apreutesei@gmail.com>
2021-11-18 01:10:45 +0200

    unimportant

--- base64.lua
+++ base64.lua
@@ -81,8 +81,6 @@ function base64.encode(s, sn, dbuf, dn)
 		dp[di] = EQ
 	end
 
-	assert(di == dn-1)
-
 	if dbuf then
 		return dp, dn
 	else
@@ -126,8 +124,30 @@ end
 
 
 if not ... then
+
+	local decode = base64.decode
+	local encode = base64.encode
+
+	assert(decode'YW55IGNhcm5hbCBwbGVhc3VyZS4=' == 'any carnal pleasure.')
+	assert(decode'YW55IGNhcm5hbCBwbGVhc3VyZQ==' == 'any carnal pleasure')
+	assert(decode'YW55IGNhcm5hbCBwbGVhc3Vy' == 'any carnal pleasur')
+	assert(decode'YW55IGNhcm5hbCBwbGVhc3U=' == 'any carnal pleasu')
+	assert(decode'YW55IGNhcm5hbCBwbGVhcw==' == 'any carnal pleas')
+	assert(decode'., ? !@#$%^& \n\r\n\r YW55IGNhcm5hbCBwbGVhcw== \n\r' == 'any carnal pleas')
+
+	assert(encode'any carnal pleasure.' == 'YW55IGNhcm5hbCBwbGVhc3VyZS4=')
+	assert(encode'any carnal pleasure' == 'YW55IGNhcm5hbCBwbGVhc3VyZQ==')
+	assert(encode'any carnal pleasur' == 'YW55IGNhcm5hbCBwbGVhc3Vy')
+	assert(encode'any carnal pleasu' == 'YW55IGNhcm5hbCBwbGVhc3U=')
+	assert(encode'any carnal pleas' == 'YW55IGNhcm5hbCBwbGVhcw==')
+
+	assert(decode(encode'') == '')
+	assert(decode(encode'x') == 'x')
+	assert(decode(encode'xx') == 'xx')
+	assert(decode'.!@#$%^&*( \n\r\t' == '')
+
 	local clock = require'time'.clock
-	local b64 = require'libb64'
+	local libb64 = require'libb64'
 	local s=''
 	for i=1,1000 do s = s .. '0123456789' end
 	local n = 50000
@@ -135,7 +155,7 @@ if not ... then
 	local st = clock()
 	local encoded1
 	for i=1,n do
-		encoded1 = base64.encode(s)
+		encoded1 = encode(s)
 	end
 	local et = clock()
 	local dt = et - st
@@ -144,7 +164,7 @@ if not ... then
 	local st = clock()
 	local encoded2
 	for i=1,n do
-		encoded2 = b64.encode(s)
+		encoded2 = libb64.encode(s)
 	end
 	local et = clock()
 	local dt = et - st
@@ -152,7 +172,7 @@ if not ... then
 
 	encoded2 = encoded2:gsub('\n', '')
 	assert(encoded1 == encoded2)
-	assert(base64.decode(encoded1) == s)
+	assert(decode(encoded1) == s)
 
 	--TODO: rewrite decode with ffi.
 	--TODO: benchmark decode.
